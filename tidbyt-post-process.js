00def956a573573db8001b604e14e6a34d2edfb5
Nicholas Penree <nick@penree.com>
2023-02-22 18:31:29 -0500

    Add Tidbyt post process
    This can be used in device.post.js files like this:
    
    module.exports = require(require('path').join(module.parent.path, 'tidbyt-post-process'));

---
+++ tidbyt-post-process.js
@@ -0,0 +1,68 @@
+const { debuglog } = require('util');
+const { spawn } = require("child_process");
+
+const debug = debuglog("smart-matrix-server:post-process:tidbyt");
+
+const { APPLET_FOLDER = '/applets' } = process.env;
+
+module.exports = async function onAppletRender(applet) {
+    const { name, tidbyts = [] } = applet;
+
+    for (let i = 0; i < tidbyts.length; i++) {
+        const deviceId = tidbyts[i].deviceId;
+        const apiToken = tidbyts[i].apiToken;
+        const installationId = tidbyts[i].installationId;
+        const background = !!tidbyts[i].background;
+
+        const pushArgs = [
+            'push',
+            deviceId,
+            `${APPLET_FOLDER}/${name}/${name}.webp`
+        ];
+
+        if (apiToken) {
+            pushArgs.push('--api-token');
+            pushArgs.push(apiToken);
+        }
+
+        if (installationId) {
+            pushArgs.push('--installation-id');
+            pushArgs.push(installationId);
+        }
+
+        if (background) {
+            pushArgs.push('--background');
+        }
+
+        debug(`pushing ${name} to ${tidbyts[i].deviceId}${background ? ' in the background' : ''}...`);
+
+        const pushCommand = spawn('pixlet', pushArgs);
+    
+        const timeout = setTimeout(() => {
+            console.log(`Rendering timed out for ${name}`);
+            try {
+            process.kill(pushCommand.pid, 'SIGKILL');
+            } catch (e) {
+            console.log('Could not kill process ^', e);
+            }
+        }, 20000);
+
+        pushCommand.stdout.on('data', (data) => {
+            outputError += data
+        })
+
+        pushCommand.stderr.on('data', (data) => {
+            outputError += data
+        })
+    
+        pushCommand.on('close', (code) => {
+            clearTimeout(timeout);
+            if(code === 0) {
+                debug(`pushed ${name} to ${deviceId} successfully!`);
+            } else {
+                console.error(outputError);
+                reject("Applet failed to push.");
+            }
+        });
+    }
+};
